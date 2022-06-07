df49339002b60aa9fd747de288f9f413375777bd
Michael Santos Masanet <71571114+AwesomeWolf22@users.noreply.github.com>
2022-06-07 19:00:46 -0300

    Add files via upload

---
+++ js/js/menu.js
@@ -0,0 +1,9 @@
+//Menu MOBILE
+function dropMenu() {
+    var x = document.getElementById("myDropdown");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
