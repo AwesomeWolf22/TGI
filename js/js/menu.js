335ad228903d02c1dd2f9b42a43717c9fede2ac0
Michael Santos Masanet <71571114+AwesomeWolf22@users.noreply.github.com>
2022-06-07 19:04:25 -0300

    Delete js directory

--- js/js/menu.js
+++
@@ -1,9 +0,0 @@
-//Menu MOBILE
-function dropMenu() {
-    var x = document.getElementById("myDropdown");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
